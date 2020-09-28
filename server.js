e49663d43938a1f89125140def7ed3d0a89a2b11
unknown <matejzmaj12@gmail.com>
2020-09-28 18:22:59 +0200

    Backend push

---
+++ server.js
@@ -0,0 +1,85 @@
+//importing
+import express from 'express';
+import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
+import Pusher from 'pusher';
+import cors from 'cors';
+
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+  appId: '1080560',
+  key: '03684726879b086fa396',
+  secret: '9ef6f7509b497b05b75e',
+  cluster: 'eu',
+  encrypted: true,
+});
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+//DB config
+const connection_url =
+  'mongodb+srv://admin:admin@cluster0.2lso6.mongodb.net/whatsappdb?retryWrites=true&w=majority';
+
+mongoose.connect(connection_url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('DB connected');
+
+  const msgCollection = db.collection('messagecontents');
+  const changeStream = msgCollection.watch();
+
+  changeStream.on('change', (change) => {
+    console.log(change);
+
+    if (change.operationType === 'insert') {
+      const messageDetails = change.fullDocument;
+      pusher.trigger('messages', 'inserted', {
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+      });
+    } else {
+      console.log('Error triggering Pusher');
+    }
+  });
+});
+
+//api routes
+app.get('/', (req, res) => res.status(200).send('Hello world'));
+
+app.get('/messages/sync', (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post('/messages/new', (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+// listener
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
